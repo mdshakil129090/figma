914a139b5d2baaa3f45c06f7574bb7886e030745
mdshakil129090 <159450943+mdshakil129090@users.noreply.github.com>
2024-08-29 23:27:48 +0600

    Add files via upload

---
+++ java sc/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:40,
+        nav:true,
+        items:2,
+        autoplay:true,
+        autoplayTimeout:2000,
+    });
+
+});
